f8bc2a2648d945bb4b40716f92f01e14c9d0d3a4
Koushik Pulavarthi <koushipulavarthi@gmail.com>
2026-02-15 15:03:02 +0530

    fix port

--- server.js
+++ server.js
@@ -44,10 +44,11 @@ mongoose.connect(process.env.MONGO_URL)
     //seedfunction calling
     await seedInvoice();
 
-    app.listen(process.env.PORT,()=>
-    {
-        console.log(`Server running on port ${process.env.PORT}`)
-    });
+    const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 })
 .catch((err)=>console.log(err));
 
