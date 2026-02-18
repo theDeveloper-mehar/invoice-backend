ff1cd1b642040d9974c776c610b33f71f08358b7
Koushik Pulavarthi <koushipulavarthi@gmail.com>
2026-02-18 16:55:04 +0530

    updated uptime monitor

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 
+app.get("/", (req, res) => {
+  res.status(200).send("Server is running");
+});
+
 app.use("/api/invoices",require("./routes/invoiceRoutes"));
 app.use("/api/auth",require("./routes/authRoutes"));
 
